4df6ded34ef816c85a0cfb822f5f8541b285a921
godlixe <allzllx19@gmail.com>
2022-09-14 15:19:12 +0700

    square

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ function main(){
     var canvas = document.getElementById("kanvas")
     var gl = canvas.getContext('webgl')
 
+    var vertices = [0.5, 0.5, 0.0, 0.0, -0.5, 0.5, 0.0, 1.0];
+
+    var buffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
     //  Vertex shader
     var vertexShaderCode 
-    = `void main(){
-        float x = 0.0;
-        float y = 0.0;
+    = `
+    attribute vec2 aPosition;
+    void main(){
+        float x = aPosition.x;
+        float y = aPosition.y;
         gl_PointSize = 10.0;
-        gl_Position = vec4(x, 0.0, 0.0, 1.0);
+        gl_Position = vec4(aPosition.xy, 0.0, 1.0);
     }`
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vertexShaderObject, vertexShaderCode)
@@ -34,10 +41,14 @@ function main(){
     gl.linkProgram(shaderProgram)
     gl.useProgram(shaderProgram)
 
-            gl.clearColor(1, 0.65, 0.0, 1.0)
+
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition")
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(aPosition)
+    gl.clearColor(1, 0.65, 0.0, 1.0)
     // Merah Hijau Biru Transparansi
 
-        gl.clear(gl.COLOR_BUFFER_BIT)
-        gl.drawArrays(gl.POINTS, 0, 1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
 
 }
